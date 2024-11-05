f76a35fa1ca4de80fbd1b674d908809d0c7bf486
olitreadwell <oliver.treadwell@gmail.com>
2024-11-06 01:37:48 +1300

    Adjust enemy movement speed and spawn timing; enhance targeting logic in BaseTower

--- Assets/Scripts/Tower/BaseTower.cs
+++ Assets/Scripts/Tower/BaseTower.cs
@@ -14,22 +14,55 @@ public class BaseTower : MonoBehaviour
     [Header("Targeting")]
     public LayerMask targetLayer;
     public Collider[] targetsInRange;
+    public List<EnemyController> enemiesInRange = new List<EnemyController>();
+    // last time we checked for new targets
+    public float lastTargetCheck;
+    // time between checking for new targets
+    public float targetCheckRate = 0.2f; // 5 times per second not every frame
     public Collider currentTarget;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastTargetCheck = targetCheckRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        FindTargets();
+        // count down the time until we check for new targets
+        lastTargetCheck -= Time.deltaTime;
+        if (lastTargetCheck > 0)
+        {
+            Debug.Log("Not time to check for new targets yet");
+            return;
+        }
 
+        FindTargets();
+        FindEnemiesInRange();
     }
 
     private void FindTargets() {
         targetsInRange = Physics.OverlapSphere(transform.position, attackRange, targetLayer);       
+    }
+
+    private void FindEnemiesInRange() {
+        enemiesInRange.Clear();
+        foreach (Collider target in targetsInRange)
+        {
+            var enemy = target.GetComponent<EnemyController>();
+            if (enemy == null)
+            {
+                Debug.LogError("EnemyController not found on target");
+                return;
+            }
+            // if (enemy != null)
+            // {
+                enemiesInRange.Add(enemy);
+            // }
+        }
     }
 }
