c8a3b568409497118bdae1dc09914c1a245b448b
olitreadwell <oliver.treadwell@gmail.com>
2024-11-05 21:01:45 +1300

    Refactor SimpleEnemySpawner to improve spawn logic; rename variables for clarity and streamline enemy spawning process

--- Assets/Scripts/SimpleEnemySpawner.cs
+++ Assets/Scripts/SimpleEnemySpawner.cs
@@ -4,46 +4,50 @@ using UnityEngine;
 
 public class SimpleEnemySpawner : MonoBehaviour
 {
-    
+
+    [Header("Enemy Settings")]    
     public EnemyController enemyToSpawn;
     public Transform spawnPoint;
+    public int totalEnemiesToSpawn = 15;
+
+    [Header("Spawn Timing")]
     public float timeBetweenSpawns = 1f;
-    private float spawnCounter;
-    public int amountToSpawn = 15;
+    private float spawnTimer;
     // Start is called before the first frame update
     void Start()
     {
-        spawnCounter = timeBetweenSpawns;
+        spawnTimer = timeBetweenSpawns;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Check if we have any enemies left to spawn
-        if (amountToSpawn > 0)
+        if (totalEnemiesToSpawn > 0)
         {
             // Count down the spawn counter
-            spawnCounter -= Time.deltaTime;
+            spawnTimer -= Time.deltaTime;
 
             // Check if it is time to spawn an enemy
-            if (spawnCounter <= 0)
+            if (spawnTimer <= 0)
             {
-                // Reset the spawn counter
-                spawnCounter = timeBetweenSpawns;
-
                 // Spawn an enemy
                 SpawnEnemy();
 
+                // Reset the spawn counter
+                spawnTimer = timeBetweenSpawns;
             }
         }
     }
 
+
+    // Function to spawn an enemy
     private void SpawnEnemy()
     {
         // Spawn an enemy
-        EnemyController newEnemy = Instantiate(enemyToSpawn, spawnPoint.position, spawnPoint.rotation);
+        Instantiate(enemyToSpawn, spawnPoint.position, spawnPoint.rotation);
 
         // Reduce the amount of enemies left to spawn
-        amountToSpawn--;
+        totalEnemiesToSpawn--;
     }
 }
