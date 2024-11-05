2a15f5026789df33558b4cb07a9532392de2b5b1
olitreadwell <oliver.treadwell@gmail.com>
2024-11-05 15:41:59 +1300

    Add health display UI for Castle and update health management logic

--- Assets/Scripts/Castle.cs
+++ Assets/Scripts/Castle.cs
@@ -1,16 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Castle : MonoBehaviour
 {
     public float totalHealth = 100;
     private float currentHealth;
+    private Label healthValueLabel;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = totalHealth;
+
+        var uiDocument = GetComponentInChildren<UIDocument>();
+        if(uiDocument != null)
+        {
+            Debug.Log("Found UI Document");
+            
+            var rootVisualElement = uiDocument.rootVisualElement;
+
+            healthValueLabel = rootVisualElement.Q<Label>("healthValueLabel");
+
+            if (healthValueLabel != null)
+            {
+                Debug.Log("Found Health Value Label");
+                UpdateHealthDisplay();
+            } else
+            {
+                Debug.Log("Health Value Label not found");
+            }
+        } else
+        {
+            Debug.Log("UI Document not found");
+        }
     }
 
     // Update is called once per frame
@@ -28,9 +52,21 @@ public class Castle : MonoBehaviour
         if(currentHealth <= 0)
         {
             currentHealth = 0;
-            
+
             // Destroy the castle
             gameObject.SetActive(false);
+        }
+
+        // Update the health display
+        UpdateHealthDisplay();
+    }
+
+    private void UpdateHealthDisplay()
+    {
+        // Update the health display
+        if (healthValueLabel != null)
+        {
+            healthValueLabel.text = "Health: " + currentHealth.ToString();
         }
     }
 }
