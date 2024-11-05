152970b32b70696bea2b99e1dc6b950c84d15760
olitreadwell <oliver.treadwell@gmail.com>
2024-11-06 01:22:44 +1300

    Add attack attributes and targeting logic to BaseTower; update Enemy prefab layer and add collider

--- Assets/Scripts/Tower/BaseTower.cs
+++ Assets/Scripts/Tower/BaseTower.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class BaseTower : MonoBehaviour
 {
+
+    [Header("Tower Attributes")]
+    public float attackRange = 5f;
+    public float attackDamage = 10f;
+    public float attackSpeed = 1f;
+
+
+    [Header("Targeting")]
+    public LayerMask targetLayer;
+    public Collider[] targetsInRange;
+    public Collider currentTarget;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,6 @@ public class BaseTower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        targetsInRange = Physics.OverlapSphere(transform.position, attackRange, targetLayer);
     }
 }
